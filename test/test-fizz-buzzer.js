475794df4397578802e5d24627eac78166da557a
Kevin Nichols <kevinlnichols@gmail.com>
2017-10-11 12:25:47 -0400

    updated code

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -6,38 +6,24 @@ describe('fizzBuzzer', function() {
 
   // test the normal case
   it('should have remainder of 0 if divided by 15', function() {
-    const normalCases = [
-      {num: 15, expected: 0},
-      {num: 120, expected: 0},
-      {num: 240, expected: 0}
-    ];
-    normalCases.forEach(function(input) {
-      const answer = fizzBuzzer(input.num);
-      answer.should.equal(input.expected);
+    [15, 30, 45].forEach(function(input) {
+      fizzBuzzer(input).should.equal('fizz-buzz');
     });
   });
 
   it('should have remainder of 0 if divided by 5', function() {
-    const normalCases = [
-      {num: 15, expected: 0},
-      {num: 5, expected: 0},
-      {num: 240, expected: 0}
-    ];
+    const normalCases = [5, 10, 20];
     normalCases.forEach(function(input) {
-      const answer = fizzBuzzer(input.num);
-      answer.should.equal(input.expected);
+      const answer = fizzBuzzer(input);
+      answer.should.equal('buzz');
     });
   });
 
 it('should have remainder of 0 if divided by 3', function() {
-    const normalCases = [
-      {num: 15, expected: 0},
-      {num: 3, expected: 0},
-      {num: 240, expected: 0}
-    ];
+    const normalCases = [3, 6, 9, 12];
     normalCases.forEach(function(input) {
-      const answer = fizzBuzzer(input.num);
-      answer.should.equal(input.expected);
+      const answer = fizzBuzzer(input);
+      answer.should.equal('fizz');
     });
   });
 
